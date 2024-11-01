c072144a2dce450fbdb1709fa8ff77ac722956fe
Michael Tran <michael.tran@sandstone.com.au>
2024-11-01 12:45:02 +1100

    Initial commit

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,32 +1,44 @@
-import { Component, Prop, h } from '@stencil/core';
-import { format } from '../../utils/utils';
+import {
+  Component,
+  h,
+  Host,
+  Element,
+} from '@stencil/core';
+import { Collapse } from 'bootstrap/dist/js/bootstrap.esm.js';
 
 @Component({
   tag: 'my-component',
-  styleUrl: 'my-component.css',
-  shadow: true,
+  scoped: true,
 })
 export class MyComponent {
-  /**
-   * The first name
-   */
-  @Prop() first: string;
 
-  /**
-   * The middle name
-   */
-  @Prop() middle: string;
+  @Element() hostElement: HTMLElement;
 
-  /**
-   * The last name
-   */
-  @Prop() last: string;
-
-  private getText(): string {
-    return format(this.first, this.middle, this.last);
-  }
+  collapse: Collapse;
 
   render() {
-    return <div>Hello, World! I'm {this.getText()}</div>;
+    return (
+      <Host>
+        <p class="d-inline-flex gap-1">
+          <button class="btn btn-primary" type="button" aria-expanded="false" aria-controls="collapseExample">
+            Button with data-bs-target
+          </button>
+        </p>
+        <div class="collapse" id="collapseExample">
+          <div class="card card-body">
+            Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+          </div>
+        </div>
+      </Host>
+    );
+  }
+
+  componentDidRender() {
+    const collapseBtn = this.hostElement.querySelector('button');
+    collapseBtn.setAttribute('data-bs-toggle', 'collapse');
+    collapseBtn.setAttribute('data-bs-target', '#collapseExample');
+
+    const collapseElement = this.hostElement.querySelector('#collapseExample');
+    this.collapse = Collapse.getOrCreateInstance(collapseElement, { toggle: false });
   }
 }
